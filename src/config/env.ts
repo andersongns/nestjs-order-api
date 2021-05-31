27996c9c5321666f348f3390a565419ae35209b5
Anderson Santos <anderson.santos@tuta.io>
2021-05-31 14:03:35 -0300

    chore: add dotenv

---
+++ src/config/env.ts
@@ -0,0 +1,15 @@
+import { config } from 'dotenv';
+
+config();
+
+export default {
+  mode: process.env.NODE_ENV,
+  port: Number(process.env.PORT) || 3000,
+  database: {
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+  },
+};
