c1f1db8504acf5ef9079a7bd60d56bec06e11f97
Anderson Santos <anderson.santos@tuta.io>
2021-05-31 18:12:37 -0300

    chore: configure typeorm

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,20 @@
+import { ConnectionOptions } from 'typeorm';
+import env from './env';
+
+const typeOrmConfig: ConnectionOptions = {
+  type: 'mysql',
+  host: env.database.host,
+  port: env.database.port,
+  username: env.database.username,
+  password: env.database.password,
+  database: env.database.database,
+  migrationsTableName: 'order_api_migration',
+  migrations: ['database/migrations/*.{js,ts}'],
+  cli: {
+    migrationsDir: 'database/migrations',
+  },
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: false,
+};
+
+export = typeOrmConfig;
